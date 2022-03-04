a3a2a70cc614631bb191f4b3e829625a9be4bfc2
valerius <zhaohui61210@hotmail.com>
2022-03-04 18:22:17 +0800

    add cache when redis outage

--- src/main/java/com/example/tiny_demo/security/aspect/RedisCacheAspect.java
+++ src/main/java/com/example/tiny_demo/security/aspect/RedisCacheAspect.java
@@ -1,6 +1,7 @@
 package com.example.tiny_demo.security.aspect;
 
 import com.example.tiny_demo.security.anotation.CustomCacheException;
+
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Around;
@@ -10,6 +11,7 @@ import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.annotation.Order;
+import org.springframework.data.redis.RedisConnectionFailureException;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Method;
@@ -24,9 +26,10 @@ import java.lang.reflect.Method;
 public class RedisCacheAspect {
     private static final Logger logger = LoggerFactory.getLogger(RedisCacheAspect.class);
 
+    private static boolean skipRedisCache = false;
+
     // com.example.tiny_demo.modules.ums.service.impl
     @Pointcut("execution(public * com.example.tiny_demo.modules..service..*CacheService.*(..))")
-//    @Pointcut("execution(public * com.example.tiny_demo.modules.ums.service.impl.UmsAdminCacheServiceImpl.*(..))")
     public void cacheAspect() {
     }
 
@@ -40,22 +43,33 @@ public class RedisCacheAspect {
         Object result = null;
         try {
             logger.debug("cache related  intercept");
+            // 新增跳过redis缓存
+            if(skipRedisCache) {
+                logger.debug("current skip RedisCache");
+                return null;
+            }
             result = joinPoint.proceed();
+        } catch (RedisConnectionFailureException redisConnectionException){
+            //  若启动应用后第一次使用redis缓存就出现 RedisConnectionException，说明了redis服务没启动，则以后的每次缓存请求都直接跳过，即停止每次尝试连接redis
+            // TODO 此外，应设置一个定时任务，多长时间后恢复尝试redis连接,即将skipRedisCache设为false即可
+            skipRedisCache = true;
+            logger.warn(redisConnectionException.getMessage());
+            logger.warn("if firstly Unable to connect to Redis, the rest of cache requests skip");
         } catch (Throwable throwable) {
             //有CacheException注解的类打印异常就可
             if(curInvocationClass.isAnnotationPresent(CustomCacheException.class)) {
                 logger.debug("redis related while class with CustomCacheException");
             }
-            //有CacheException注解的方法
+            //有CacheException注解的方法, 实际上后续都是在cache类上加注解，不在cache类方法上加注解
             else if (method.isAnnotationPresent(CustomCacheException.class)) {
                 logger.debug("redis related  while method with CustomCacheException");
-                // 此处不抛出异常，否则影响除去redis的正常功能
+                // 此处不抛出异常，否则影响非redis的正常功能
 //                throw throwable;
             } else {
                 logger.debug("other exception");
             }
             // 打印以下异常即可，跳过redis继续执行其他
-            logger.error(throwable.getMessage());
+            logger.warn(throwable.getMessage());
         }
         return result;
     }
